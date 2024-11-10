4c2eba54162ce0212e8673d65b89435e3edb4ed5
Aaryan Dhand <aaryandhand@gmail.com>
2024-11-10 04:22:28 -0700

    persistent tracking of json

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import os
-
 import boto3
+import subprocess
 from dotenv import load_dotenv
 from eventposting import eventpostingroutes
 from flask import Flask, g
@@ -17,8 +17,7 @@ CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 # Load AWS configuration from environment variables
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-REGION_NAME = 'us-east-2'  # os.getenv('REGION_NAME')
-
+REGION_NAME = 'us-east-2'
 
 @app.before_request
 def before_request():
@@ -30,12 +29,13 @@ def before_request():
         region_name=REGION_NAME
     )
 
-
 # Register blueprints with URL prefixes
 app.register_blueprint(jobpostingroutes)
 app.register_blueprint(eventpostingroutes)
 app.register_blueprint(organizationroutes)
 
-
 if __name__ == '__main__':
+    # Run `json_monitor.py` as a background process
+    subprocess.Popen(["python3", "json_monitor.py"])
+    # Start the Flask app
     app.run(debug=True)
